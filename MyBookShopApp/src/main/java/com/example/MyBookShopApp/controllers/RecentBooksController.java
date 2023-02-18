2aa68caa50d404a63e4de668771898f89f4fcee1
Эльнара Габдрахманова <elnaradin@gmail.com>
2023-02-19 02:24:37 +0500

    initial commit

---
+++ MyBookShopApp/src/main/java/com/example/MyBookShopApp/controllers/RecentBooksController.java
@@ -0,0 +1,29 @@
+package com.example.MyBookShopApp.controllers;
+
+import com.example.MyBookShopApp.data.Book;
+import com.example.MyBookShopApp.data.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+
+@Controller
+public class RecentBooksController {
+    private final BookService bookService;
+
+    @Autowired
+    public RecentBooksController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @ModelAttribute("booksList")
+    public List<Book> bookList(){
+        return bookService.getBooksData();
+    }
+    @GetMapping("/books/recent")
+    public String recentBooksPage(){
+        return "/books/recent";
+    }
+}
